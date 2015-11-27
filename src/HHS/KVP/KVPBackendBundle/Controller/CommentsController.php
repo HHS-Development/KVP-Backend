9a4f441a734e821ec38e84dc581aa0716f8ced02
Fabian Jungwirth <github@demofire.de>
2015-11-27 11:35:20 +0100

    FOSRestBundle configuration setup
    Configure gernal rest routes via app/config/routing.yml
    Configure project rest routes via api-routing.yml
    Add Ticket and Comment Controller
    Configure Comment Controller Parent

---
+++ src/HHS/KVP/KVPBackendBundle/Controller/CommentsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HHS\KVP\KVPBackendBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+class CommentsController extends FOSRestController
+{
+    public function postCommentAction($slug){
+
+    }
+}
---
+++ src/HHS/KVP/KVPBackendBundle/Controller/TicketsController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @author Fabian Jungwirth aka Noxaro
+ */
+
+namespace HHS\KVP\KVPBackendBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations\RouteResource;
+use FOS\RestBundle\Controller\FOSRestController;
+use HHS\KVP\KVPBackendBundle\Model\Ticket;
+
+/**
+ * Class TicketsController
+ * @package HHS\KVP\KVPBackendBundle\Controller
+ */
+class TicketsController extends FOSRestController {
+
+    /**
+     *
+     * @return Ticket
+     */
+    public function getTicketsAction(){
+
+    }
+
+    /**
+     *
+     * @param $id ticket id
+     */
+    public function getTicketAction($id){
+
+    }
+
+    /**
+     *
+     */
+    public function postTicketAction(){
+
+    }
+
+    /**
+     * @param $id ticket id
+     */
+    public function putTicketAction($id){
+
+    }
+
+    /**
+     * @param $id
+     */
+    public function deleteTicketAction($id){
+
+    }
+}
