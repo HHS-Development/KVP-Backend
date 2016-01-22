3e9875314b62e5b04c10c276fd844c2cdae668a3
Fabian Jungwirth <github@demofire.de>
2016-01-22 09:35:54 +0100

    Add bad request response if the form is invalid

--- src/HHS/KVP/KVPBackendBundle/Controller/CommentsController.php
+++ src/HHS/KVP/KVPBackendBundle/Controller/CommentsController.php
@@ -16,6 +16,7 @@ use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * Class CommentsController
@@ -80,11 +81,11 @@ class CommentsController extends FOSRestController
         if ($form->isValid()) {
             $entityManager->persist($ticketComment);
             $entityManager->flush();
+            $view = $this->view($ticketComment, 200);
+            return $this->handleView($view);
+        } else {
+            return new BadRequestHttpException();
         }
-
-
-        $view = $this->view($ticketComment, 200);
-        return $this->handleView($view);
     }
 
     /**
